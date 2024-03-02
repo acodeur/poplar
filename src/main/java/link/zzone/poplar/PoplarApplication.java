a02162ec8a0850cf593f545df3399ca6a98c4155
Chris Chen <y_zhengchen@163.com>
2024-03-02 23:39:50 +0800

    first commit for initial service framework

---
+++ src/main/java/link/zzone/poplar/PoplarApplication.java
@@ -0,0 +1,16 @@
+package link.zzone.poplar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chrischen
+ */
+@SpringBootApplication
+public class PoplarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoplarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/zzone/poplar/PoplarApplicationTests.java
@@ -0,0 +1,13 @@
+package link.zzone.poplar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoplarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
